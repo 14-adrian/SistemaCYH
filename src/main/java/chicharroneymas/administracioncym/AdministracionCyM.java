d7785c7bc04b8aca01e6646ca233685b70b69ffd
Leoni <Leoni@WARMACHINE>
2024-03-27 11:15:17 -0600

    nuevo

---
+++ src/main/java/chicharroneymas/administracioncym/AdministracionCyM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package chicharroneymas.administracioncym;
+
+/**
+ *
+ * @author Leoni
+ */
+public class AdministracionCyM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
